79991a2dbab860eb4b49cfce4af63dc7f258c457
yuki-496 <worksmerry96@gmail.com>
2024-12-07 20:56:49 +0900

    day6_rails-homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
 end
+
+# HomeControllerが継承しているApplicationController(ActionController::Base)には
+# 膨大なコード/定義（例：renderメソッド、beforeアクション、フィルターなど）が入っている！
+
