2accc48eee2d971b80209ff975cd98fc4349d6fb
yuki-496 <worksmerry96@gmail.com>
2024-12-08 16:35:16 +0900

    DAY07-Articleモデルを作成(復習)

--- db/migrate/20241208040157_create_articles.rb
+++ db/migrate/20241208040157_create_articles.rb
@@ -1,9 +1,16 @@
+# マイグレーションは、データを保存するためのテーブルを作るためのファイル
+# ターミナル(Ubuntu)で「rails g model Article(モデル名)」でモデルを作成すると、同時にマイグレーションも作られる！
+#                            ↑ 'g'は 'generate'の略！
+
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :title
+      # ↓テーブルにカラムを追加する
+      t.string :title # tはtableのt, stringはデータ型, :titleはカラム名
       t.text :content
-      t.timestamps
+      t.timestamps # お決まり表現。そのデータが追加された日付を保存するカラムを追加
     end
   end
 end
+
+
